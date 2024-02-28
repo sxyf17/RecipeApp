84ee2bea30607ebae1b5c8b64bb78cad7be725f4
saif farooqi <farooqisaif1@gmail.com>
2024-02-27 21:05:27 -0500

    homepage UI v1

---
+++ lib/home.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class RecipeHome extends StatefulWidget {
+  const RecipeHome({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<RecipeHome> createState() => _RecipeHomeState();
+}
+
+class _RecipeHomeState extends State<RecipeHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/app_logo.webp',
+              height: 75,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Welcome to Recipe App!',
+              style: TextStyle(fontSize: 25),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/images/food_icon.jpeg',
+                    fit: BoxFit.fill,
+                  )),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            
+            const SizedBox(
+              width: 250,
+              child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ), 
+                  hintText: 'Username...',),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const SizedBox(
+              width: 250,
+              child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ), hintText: 'Password...'),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple[100]), child: const Text('Log In')),
+            const SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple[100]), child: const Text('Sign Up'))
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
